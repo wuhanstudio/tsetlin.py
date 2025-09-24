864dc77f93da92dd26bc07ec4a9823253674a1e0
wuhanstudio <wuhanstudios@gmial.com>
2025-09-24 14:23:39 +0100

    [optuna] Add optuna

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import random
+random.seed(0)
+
 import argparse
 from loguru import logger
 
@@ -25,6 +28,24 @@ X = iris[["sepal_length", "sepal_width", "petal_length", "petal_width"]].to_nump
 X_mean = mean(X)
 X_std = std(X)
 
+X_train, X_test, y_train, y_test = None, None, None, None
+
+def objective(trial):
+    n_state = trial.suggest_int("n_state", 2, 100, step=2)
+    n_clause = trial.suggest_int("n_clause", 2, 100, step=2)
+
+    T = trial.suggest_int("T", 1, n_state)
+    s = trial.suggest_float("s", 1.0, 10.0, step=0.1)
+
+    tsetlin = Tsetlin(N_feature=len(X_train[0]), N_class=3, N_clause=n_clause, N_state=n_state)
+
+    for epoch in range(N_EPOCHS):
+        for i in tqdm(range(len(X_train))):
+            tsetlin.step(X_train[i], y_train[i], T=T, s=s)
+
+    y_pred = tsetlin.predict(X_test)
+    return 1.0 - (sum(y_pred == y_test) / len(y_test))
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Tsetlin Machine on Iris Dataset")
     parser.add_argument("--epochs", type=int, default=10, help="Number of training epochs")
@@ -36,60 +57,71 @@ if __name__ == "__main__":
     parser.add_argument("--T", type=int, default=30, help="Threshold T")
     parser.add_argument("--s", type=float, default=6.0, help="Specificity s")
 
+    parser.add_argument("--optuna", action='store_true')
     args = parser.parse_args()
 
     N_EPOCHS = args.epochs
-    N_CLAUSE = args.n_clause
-    N_STATE  = args.n_state
 
     N_BIT = args.n_bit
     if N_BIT not in {1, 2, 4, 8}:
         raise ValueError("n_bit must be one of [1, 2, 4, 8]")
 
     logger.debug(f"Using {N_BIT} bits for booleanization")
-    logger.debug(f"Number of clauses: {N_CLAUSE}, Number of states: {N_STATE}")
-    logger.debug(f"Threshold T: {args.T}, Specificity s: {args.s}")
-
     X_bool = booleanize_features(X, X_mean, X_std, num_bits=N_BIT)
 
     # Train-test split
     X_train, X_test, y_train, y_test = train_test_split(X_bool, y, test_size=0.2, random_state=0)
 
-    tsetlin = Tsetlin(N_feature=len(X_train[0]), N_class=3, N_clause=N_CLAUSE, N_state=N_STATE)
+    if args.optuna:
+        import optuna
+        
+        # Create a new study.
+        study = optuna.create_study()  
+        
+        # Invoke optimization of the objective function.
+        study.optimize(objective, n_trials=20)  
+    else:
+        N_CLAUSE = args.n_clause
+        N_STATE  = args.n_state
 
-    y_pred = tsetlin.predict(X_test)
-    accuracy = sum(y_pred == y_test) / len(y_test)
+        logger.debug(f"Number of clauses: {N_CLAUSE}, Number of states: {N_STATE}")
+        logger.debug(f"Threshold T: {args.T}, Specificity s: {args.s}")
 
-    for epoch in range(N_EPOCHS):
-        logger.info(f"[Epoch {epoch+1}/{N_EPOCHS}] Train Accuracy: {accuracy * 100:.2f}%")
-        for i in tqdm(range(len(X_train))):
-            tsetlin.step(X_train[i], y_train[i], T=args.T, s=args.s)
+        tsetlin = Tsetlin(N_feature=len(X_train[0]), N_class=3, N_clause=N_CLAUSE, N_state=N_STATE)
 
-        y_pred = tsetlin.predict(X_train)
-        accuracy = sum(y_pred == y_train) / len(y_train)
+        y_pred = tsetlin.predict(X_test)
+        accuracy = sum(y_pred == y_test) / len(y_test)
 
-    # tsetlin.fit(X_train, y_train, T=15, s=3, epochs=EPOCHS)
+        for epoch in range(N_EPOCHS):
+            logger.info(f"[Epoch {epoch+1}/{N_EPOCHS}] Train Accuracy: {accuracy * 100:.2f}%")
+            for i in tqdm(range(len(X_train))):
+                tsetlin.step(X_train[i], y_train[i], T=args.T, s=args.s)
 
-    logger.info("")
+            y_pred = tsetlin.predict(X_train)
+            accuracy = sum(y_pred == y_train) / len(y_train)
 
-    # Final evaluation
-    y_pred = tsetlin.predict(X_test)
-    accuracy = sum(y_pred == y_test) / len(y_test)
+        # tsetlin.fit(X_train, y_train, T=15, s=3, epochs=EPOCHS)
 
-    logger.info(f"Test Accuracy: {accuracy * 100:.2f}%")
+        logger.info("")
 
-    # Save the model
-    tsetlin.save_model("tsetlin_model.pb", type="training")
-    logger.info("Model saved to tsetlin_model.pb")
+        # Final evaluation
+        y_pred = tsetlin.predict(X_test)
+        accuracy = sum(y_pred == y_test) / len(y_test)
 
-    logger.info("")
+        logger.info(f"Test Accuracy: {accuracy * 100:.2f}%")
 
-    # Load the model
-    n_tsetlin = Tsetlin.load_model("tsetlin_model.pb")
-    logger.info("Model loaded from tsetlin_model.pb")
+        # Save the model
+        tsetlin.save_model("tsetlin_model.pb", type="training")
+        logger.info("Model saved to tsetlin_model.pb")
 
-    # Evaluate the loaded model
-    n_y_pred = n_tsetlin.predict(X_test)
-    accuracy = sum(n_y_pred == y_test) / len(y_test)
+        logger.info("")
 
-    logger.info(f"Test Accuracy (Loaded Model): {accuracy * 100:.2f}%")
+        # Load the model
+        n_tsetlin = Tsetlin.load_model("tsetlin_model.pb")
+        logger.info("Model loaded from tsetlin_model.pb")
+
+        # Evaluate the loaded model
+        n_y_pred = n_tsetlin.predict(X_test)
+        accuracy = sum(n_y_pred == y_test) / len(y_test)
+
+        logger.info(f"Test Accuracy (Loaded Model): {accuracy * 100:.2f}%")
